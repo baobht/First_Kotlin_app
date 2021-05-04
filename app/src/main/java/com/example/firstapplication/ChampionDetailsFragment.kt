3d8c98ffc506d98753331e73371477e5817f1973
baobht <binpn1603@gmail.com>
2021-05-04 22:45:48 +0700

    Update UI and database

--- app/src/main/java/com/example/firstapplication/ChampionDetailsFragment.kt
+++ app/src/main/java/com/example/firstapplication/ChampionDetailsFragment.kt
@@ -31,6 +31,15 @@ class ChampionDetailsFragment : Fragment(R.layout.champ_details) {
 
     private lateinit var skillTi: TextView
     private lateinit var skillDe: TextView
+    private lateinit var champCost: TextView
+    private lateinit var champTrait1: TextView
+    private lateinit var champTrait2: TextView
+    private lateinit var champTrait3: TextView
+    private lateinit var champTrait4: TextView
+    private lateinit var imgTrait1: ImageView
+    private lateinit var imgTrait2: ImageView
+    private lateinit var imgTrait3: ImageView
+    private lateinit var imgTrait4: ImageView
     private lateinit var imageSource: ImageView
     private lateinit var skillSource: ImageView
 
@@ -58,14 +67,23 @@ class ChampionDetailsFragment : Fragment(R.layout.champ_details) {
         skillDe = view.findViewById(R.id.tvChampSkillD)
         imageSource = view.findViewById(R.id.tvChampImg)
         skillSource = view.findViewById(R.id.tvSkillImg)
-        champDet = view.findViewById(R.id.tvChampDet)
+        champTrait1 = view.findViewById(R.id.txtTrait1)
+        champTrait2 = view.findViewById(R.id.txtTrait2)
+        champTrait3 = view.findViewById(R.id.txtTrait3)
+        champTrait4 = view.findViewById(R.id.txtTrait4)
+        champCost = view.findViewById(R.id.txtCost)
+        imgTrait1 = view.findViewById(R.id.imgTrait1)
+        imgTrait2 = view.findViewById(R.id.imgTrait2)
+        imgTrait3 = view.findViewById(R.id.imgTrait3)
+        imgTrait4 = view.findViewById(R.id.imgTrait4)
+
+//        println((champion.traits)[0])
 
 
         // set details to layout
         champName.text = champion.name
         skillTi.text = champion.skillTitle
         skillDe.text = champion.skillDes
-        champDet.text = champion.skillDes
         var skillImg = champion.name.replace("\\s+|'".toRegex(), "").toLowerCase()
         var imgName = champion.name.replace("\\s+|'".toRegex(), "")
         Picasso.get()
@@ -75,7 +93,57 @@ class ChampionDetailsFragment : Fragment(R.layout.champ_details) {
         Picasso.get()
             .load("https://raw.githubusercontent.com/baobht/First_Kotlin_app/master/app/set5/championskills/"+skillImg+"_passive.png")
             .into(skillSource)
-
+        when((champion.traits).size){
+            1 -> {
+                champTrait1.text = (champion.traits)[0].replace("Set5_","")
+                Picasso.get()
+                    .load("https://raw.githubusercontent.com/baobht/First_Kotlin_app/master/app/set5/traits/"+(champion.traits)[0].replace("Set5_","").toLowerCase()+".png")
+                    .into(imgTrait1)
+            }
+            2 -> {
+                champTrait1.text = (champion.traits)[0].replace("Set5_","")
+                champTrait2.text = (champion.traits)[1].replace("Set5_","")
+                Picasso.get()
+                    .load("https://raw.githubusercontent.com/baobht/First_Kotlin_app/master/app/set5/traits/"+(champion.traits)[0].replace("Set5_","").toLowerCase()+".png")
+                    .into(imgTrait1)
+                Picasso.get()
+                    .load("https://raw.githubusercontent.com/baobht/First_Kotlin_app/master/app/set5/traits/"+(champion.traits)[1].replace("Set5_","").toLowerCase()+".png")
+                    .into(imgTrait2)
+            }
+            3 -> {
+                champTrait1.text = (champion.traits)[0].replace("Set5_","")
+                champTrait2.text = (champion.traits)[1].replace("Set5_","")
+                champTrait3.text = (champion.traits)[2].replace("Set5_","")
+                Picasso.get()
+                    .load("https://raw.githubusercontent.com/baobht/First_Kotlin_app/master/app/set5/traits/"+(champion.traits)[0].replace("Set5_","").toLowerCase()+".png")
+                    .into(imgTrait1)
+                Picasso.get()
+                    .load("https://raw.githubusercontent.com/baobht/First_Kotlin_app/master/app/set5/traits/"+(champion.traits)[1].replace("Set5_","").toLowerCase()+".png")
+                    .into(imgTrait2)
+                Picasso.get()
+                    .load("https://raw.githubusercontent.com/baobht/First_Kotlin_app/master/app/set5/traits/"+(champion.traits)[2].replace("Set5_","").toLowerCase()+".png")
+                    .into(imgTrait3)
+            }
+            else -> {
+                champTrait1.text = (champion.traits)[0].replace("Set5_","")
+                champTrait2.text = (champion.traits)[1].replace("Set5_","")
+                champTrait3.text = (champion.traits)[2].replace("Set5_","")
+                champTrait4.text = (champion.traits)[3].replace("Set5_","")
+                Picasso.get()
+                    .load("https://raw.githubusercontent.com/baobht/First_Kotlin_app/master/app/set5/traits/"+(champion.traits)[0].replace("Set5_","").toLowerCase()+".png")
+                    .into(imgTrait1)
+                Picasso.get()
+                    .load("https://raw.githubusercontent.com/baobht/First_Kotlin_app/master/app/set5/traits/"+(champion.traits)[1].replace("Set5_","").toLowerCase()+".png")
+                    .into(imgTrait2)
+                Picasso.get()
+                    .load("https://raw.githubusercontent.com/baobht/First_Kotlin_app/master/app/set5/traits/"+(champion.traits)[2].replace("Set5_","").toLowerCase()+".png")
+                    .into(imgTrait3)
+                Picasso.get()
+                    .load("https://raw.githubusercontent.com/baobht/First_Kotlin_app/master/app/set5/traits/"+(champion.traits)[3].replace("Set5_","").toLowerCase()+".png")
+                    .into(imgTrait4)
+            }
+        }
+        champCost.text = champion.cost.toString()
         return view
     }
 
